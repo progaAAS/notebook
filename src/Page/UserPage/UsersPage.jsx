542aaf904056e5bb793c446dc89578ccd3ef2684
AAS <gostand8@gmail.com>
2021-10-06 14:43:41 +0300

    Second commit

--- src/Page/UserPage/UsersPage.jsx
+++ src/Page/UserPage/UsersPage.jsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Navbar from "../../component/Navbar/Navbar";
 import UsersList from "../../component/Users/UsersList/UsersList";
-import { getInfoUser } from "../../redux/userReducer";
-
+import { Redirect } from "react-router-dom";
 
 const UsersPage = React.memo(function UsersPage() {
   const dispatch = useDispatch();
+
+
+  const {isAuth} = useSelector(state => state.user);
+
+  if (!isAuth) {
+    return <Redirect to={"/"}/>
+  }
+
 
   return <div>
       <Navbar/>
